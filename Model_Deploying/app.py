9a8f8e6b085ca7fcc1c5c4baaacb723efcf5a762
Priyanshu pandey <priyanshupandey123@gmail.com>
2025-08-22 12:06:58 +0530

    first commit

---
+++ Model_Deploying/app.py
@@ -0,0 +1,76 @@
+# To run this file:
+# streamlit run "Model_Deploying/app.py"
+import streamlit as st
+import pandas as pd
+import pickle
+import numpy as np
+
+
+with open('Model_Deploying/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+df = pd.read_csv('Data/Cleanded_data.csv')
+# UI Setup
+st.title("E-Commerce Product Sales Predictor")
+st.markdown("Predict product trend based on last month's sales")
+
+# Sidebar inputs
+with st.sidebar:
+    st.header("Product Features")
+    st.subheader("Categorical Features")
+
+    stars = st.slider("star", 0.0, 5.0, 3.5, step=0.1)
+    reviews = st.number_input("reviews", min_value=0, value=100)
+    price = st.number_input("price", min_value=0.0, value=999.0)
+    category = st.selectbox("categoryName", df['categoryName'].unique())
+    product_name = st.selectbox('product_name', df['product_name'].unique())
+    is_best_seller = st.radio("Best Seller", ['True', 'False'])
+
+    def popularity(x):
+        if x >= 0 and x < 2:
+            return 'Not Popular'
+        elif x >= 2 and x < 4:
+            return 'Average Product'
+        elif x >= 4 and x <= 5:
+            return 'Popular Product'
+        else:
+            return 'unknown'
+    product_popularity = popularity(stars)
+
+    def cost_range(x):
+        if x >= 99 and x < 5000:
+            return 'Low cost '
+        if x >= 5000 and x < 20000:
+            return 'Low midrange cost'
+        if x >= 20000 and x <= 40000:
+            return 'Midrange cost'
+        if x >= 40000 and x <= 50000:
+            return 'Premium cost'
+    Product_cost_range = cost_range(price)
+    
+    product_demand = st.selectbox('DemandOfTheProduct', df['DemandOfTheProduct'].unique())
+    
+
+input_dict = {
+    'stars' : [stars],
+    'reviews' : [reviews],
+    'price' : [price],
+    'categoryName' : [category],
+    'product_name' : [product_name],
+    'Product_popularity' : [product_popularity],
+    'isBestSeller' : [is_best_seller],
+    'Product_cost_range' : [Product_cost_range],
+    'DemandOfTheProduct' : [product_demand],
+}
+
+input_df = pd.DataFrame(input_dict)
+st.write(input_df)
+
+if st.button("Predict Sales Trend", type="primary"):
+    prediction = model.predict(input_df)
+    if prediction[0] >= 400:
+        st.success(f"Yes! Product is trending in future (Future sales is: {prediction[0]:,.0f} units)")
+    elif prediction[0] >= 300:
+        st.warning(f"Yes, product has potential (Sales is: {prediction[0]:,.0f} units)")
+    else:
+        st.error(f"No, Product is not trend in future (Expected sales only {prediction[0]:,.0f} units)")
---
+++ README.md
@@ -0,0 +1,76 @@
+# Predicting Future Trends for E-Commerce Products
+
+# Project Overview
+This project analyzes Amazon product sales data to predict future product trends using machine learning. It helps identify which products are likely to be in demand next month based on historical sales data.
+
+# Problem Statement
+E-commerce companies often struggle to forecast which products will trend next. This project uses past sales data to build a model that predicts future trends and helps in inventory planning and marketing strategies.
+
+# Dataset
+- Dataset: Amazon sales data
+- Fields include: ASIN, Title, Category, Price, Ratings, Reviews, boughtInLastMonth, etc.
+- Target Column: `Bought in last month`
+
+# Technologies Used
+- Python
+- Pandas, NumPy
+- Scikit-learn
+- Matplotlib, Seaborn
+- Joblib (for model saving)
+- Streamlit (for web app)
+
+# Model Used
+- Linear Regression
+- Decision Tree Regressor
+- Random Forest Regressor
+- XGB Regressor
+- Gradient Boosting Regressor
+- Accuracy, F1-Score, Confusion Matrix used for evaluation
+
+# Folder Structure
+
+Predict future trend for E-Commerce/
+├── Data/
+│   ├── Amazone_sales_data.csv
+│   └── Cleanded_data.csv
+│
+├── Model_Deploying/
+│   ├── app.py
+│   ├── cat_encoder.pkl
+│   ├── columns.pkl  
+│   ├── cost_encoder.pkl
+│   ├── dem_encoder.pkl
+│   ├── GB_regressor.pkl
+│   ├── pop_encoder.pkl
+│   └── scaler.pkl
+│
+├── Notebooks/
+│   ├── Cleaning.ipynb
+│   └── Model_evaluation.ipynb
+│ 
+├── .gitignore
+├── requirements.txt
+└── README.md
+
+
+# How to Run
+1. Clone the repo:
+   `git clone https://github.com/PriyanshPandey/Predict future trend for E-Commerce.git`
+
+2. Navigate to folder:
+   `cd Predict future trend for E-Commerce `
+
+3. Install dependencies:
+   `pip install -r requirements.txt`
+
+4. Run the app:
+   `streamlit run app.py`
+
+# Deploy link:
+[Streamlit](https://future-trend-prediction.streamlit.app/)
+
+# Author
+- Name: Priyanshu Pandey
+- Diploma in Automation & Robotics, Govt. Polytechnic Dawahat (Almora)
+- GitHub: [github.com/yourusername](https://github.com/yourusername)
+- LinkedIn: [linkedin.com/in/yourprofile](https://linkedin.com/in/yourprofile)
